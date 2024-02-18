df07190336b2d2cd8ba1b25cee713eaeb1182ca9
Tackiee <takitt1720@gmail.com>
2024-02-18 23:44:43 +0900

    studied boxdesign

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,68 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text('Default'),
-            Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
-            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
-            Text('fontSize = 36', style: TextStyle(fontSize: 36)),
-            Text('Red', style: TextStyle(color: Colors.red)),
-            Container(
-              width: double.infinity,
-              color: Colors.grey,
-              child: Text('TextAlighn.right', textAlign: TextAlign.right),
-            ),
-          ],
+        // body: Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //   children: <Widget>[
+        //     Text('Default'),
+        //     Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
+        //     Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+        //     Text('fontSize = 36', style: TextStyle(fontSize: 36)),
+        //     Text('Red', style: TextStyle(color: Colors.red)),
+        //     Container(
+        //       width: double.infinity,
+        //       color: Colors.grey,
+        //       child: Text('TextAlighn.right', textAlign: TextAlign.right),
+        //     ),
+        //   ],
+        // ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                color: Colors.blue,
+                child: Text('blue'),
+              ),
+              Container(
+                width: 200,
+                height: 50,
+                color: Colors.blue,
+                child: Text('200x50'),
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                color: Colors.blue,
+                child: Text('padding'),
+              ),
+              Container(
+                color: Colors.blue,
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  color: Colors.green,
+                  child: Text('margin'),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.all(8),
+                child: Text('border'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage('https://placehold.jp/200x100.png'),
+                  ),
+                ),
+                width: 200,
+                height: 100,
+                child: Text('image'),
+              ),
+            ],
+          ),
         ),
       ),
     );
